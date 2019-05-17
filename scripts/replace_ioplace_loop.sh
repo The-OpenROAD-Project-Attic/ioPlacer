8424861ba8e88fca534b6f6fd2a84d04df775ae3
Vitor Bandeira <vitor.vbandeira@gmail.com>
2019-05-17 17:01:02 -0300

    Format scripts/replace_ioplace_loop.sh

--- scripts/replace_ioplace_loop.sh
+++ scripts/replace_ioplace_loop.sh
@@ -1,59 +1,120 @@
-[ ! -d /RePlAce_out ] && mkdir -p RePlAce_out;
+#!/bin/bash
 
-[ ! -d /history ] && mkdir -p history;
+cd ..
 
+ioBin="./IOPlacement/ioPlacement"
+replaceBin="./RePlAce/src/RePlAce"
+
+outputdir="outputs"
+replaceOutDir="${outputdir}/replace"
+ioplaceOutDir="${outputdir}/ioplace"
+
+mkdir -p $replaceOutDir
+mkdir -p $ioplaceOutDir
+
+if [[ $# != 1 ]]; then
+        echo "you need to give one, and only one, argument"
+        echo "the number of iterations"
+        exit 1
+fi
+
+if [[ ! -f "${ioBin}" ]]; then
+        echo "IOPlacement binary not found, you need to compile it before"
+        exit 1
+fi
+
+if [[ ! -f "${replaceBin}" ]]; then
+        echo "RePlAce binary not found, you need to compile it before"
+        exit 1
+fi
 
 # Main loop:
-for i in $(seq 2 10); # for all benchmarks
+# for all benchmarks
+for i in $(seq 2 10)
 do
-	echo "Benchmark test${i}";
-	for j in $(seq 0 $1); # do n iterations (user input) 
-	do			
-		echo "----Iteration $j";
+        echo "Benchmark test${i}"
+        curr_test="ISPD18/ispd18_test${i}/ispd18_test${i}"
+        replaceExperimentDir="${replaceOutDir}/ispd/ispd18_test${i}.input/experiment"
+        # do n iterations (user input)
+        for j in $(seq 0 "$1")
+        do
+                echo "----Iteration $j"
+                folder="${ioplaceOutDir}/it${j}/test${i}/logs"
+                mkdir -p "$folder"
+                replaceExperimentDEF=${j}/ispd18_test${i}.input_final.def
 
-		[ ! -d /history/it${j} ] && mkdir -p history/it${j};
-		
-		[ ! -d /history/it${j}/test${i} ] && mkdir -p history/it${j}/test${i};	
-		
-		[ ! -d /history/it${j}/test${i}/logs ] && mkdir -p history/it${j}/test${i}/logs;
-		
-		echo "--------Running RePlAce (for benchmark test${i})";
-		# Run RePlAce to generate a placed DEF
-		# Directory ISPD18 must have the files of ISPD18 benchmarks
-		if [ $j -eq 0 ];
-		then
-			echo "------------Creating input def with test flag";
-			./RePlAce/src/RePlAce -bmflag ispd -output RePlAce_out/ -lef ISPD18/ispd18_test${i}/ispd18_test${i}.input.lef -def ISPD18/ispd18_test${i}/ispd18_test${i}.input.def -fast > history/it${j}/test${i}/logs/replace_test${i}_it${j}.log;
-		else
-			./RePlAce/src/RePlAce -bmflag ispd -output RePlAce_out/ -lef ISPD18/ispd18_test${i}/ispd18_test${i}.input.lef -def ISPD18/ispd18_test${i}/ispd18_test${i}.input.def > history/it${j}/test${i}/logs/replace_test${i}_it${j}.log;
-		fi
+                echo "--------Running RePlAce (for benchmark test ${i})"
+                # Run RePlAce to generate a placed DEF
+                # Directory ISPD18 must have the files of ISPD18 benchmarks
+                if [[ "${j}" == 0 ]]
+                then
+                        echo "------------Creating input def with test flag"
+                        ${replaceBin} \
+                                -bmflag ispd \
+                                -output ${replaceOutDir}/ \
+                                -lef "$curr_test".input.lef \
+                                -def "$curr_test".input.def \
+                                -fast \
+                                > "$folder"/replace_test"${i}"_it"${j}".log
+                else
+                        ${replaceBin} \
+                                -bmflag ispd \
+                                -output ${replaceOutDir}/ \
+                                -lef "$curr_test".input.lef \
+                                -def "$curr_test".input.def \
+                                > "$folder"/replace_test"${i}"_it"${j}".log
+                fi
 
-		if [ ${j} -lt 10 ]; then
-			cp RePlAce_out/ispd/ispd18_test${i}.input/experiment00${j}/ispd18_test${i}.input_final.def ISPD18/ispd18_test${i}/ispd18_test${i}.input.def;
-		elif [ ${j} -lt 100 ]; then
-			cp RePlAce_out/ispd/ispd18_test${i}.input/experiment0${j}/ispd18_test${i}.input_final.def ISPD18/ispd18_test${i}/ispd18_test${i}.input.def;
-		else
-			cp RePlAce_out/ispd/ispd18_test${i}.input/experiment${j}/ispd18_test${i}.input_final.def ISPD18/ispd18_test${i}/ispd18_test${i}.input.def;
-		fi
-		
-		echo "--------Running IOPlacement";
-		# Run IOPlacement to place the IO pins for the placed DEF 
-		if [ $i -eq 2 ];
-		then
-			./IOPlacement/ioPlacement -i ISPD18/ispd18_test${i}/ispd18_test${i}.input.def -h 3 -v 4 -l 1 -o out.def > history/it${j}/test${i}/logs/IOPlacement_test${i}_it${j}.log;
-		elif [ $i -eq 3 ];
-		then
-			./IOPlacement/ioPlacement -i ISPD18/ispd18_test${i}/ispd18_test${i}.input.def -h 7 -v 6 -l 1 -o out.def > history/it${j}/test${i}/logs/IOPlacement_test${i}_it${j}.log;
-		elif [ $i -eq 4 ] || [ $i -eq 5 ];
-		then
-			./IOPlacement/ioPlacement -i ISPD18/ispd18_test${i}/ispd18_test${i}.input.def -h 7 -v 8 -l 1 -o out.def > history/it${j}/test${i}/logs/IOPlacement_test${i}_it${j}.log;
-		else
-			./IOPlacement/ioPlacement -i ISPD18/ispd18_test${i}/ispd18_test${i}.input.def -h 8 -v 7 -l 1 -o out.def > history/it${j}/test${i}/logs/IOPlacement_test${i}_it${j}.log; 
-		fi
-		
-		# Moving the output DEF of IOPlacement to the ISPD18 directory. With that, this out DEF is the new input DEF in the next iteration
-		mv out.def ISPD18/ispd18_test${i}/ispd18_test${i}.input.def;	
-	done;
-done;
+                if [ "${j}" -lt 10 ]; then
+                        cp "${replaceExperimentDir}00${replaceExperimentDEF}" \
+                                "$curr_test".input.def
+                elif [ "${j}" -lt 100 ]; then
+                        cp "${replaceExperimentDir}0${replaceExperimentDEF}" \
+                                "$curr_test".input.def
+                else
+                        cp "${replaceExperimentDir}${replaceExperimentDEF}" \
+                                "$curr_test".input.def
+                fi
 
-	
+                echo "--------Running IOPlacement"
+                # Run IOPlacement to place the IO pins for the placed DEF
+                ioLogDir="${folder}/IOPlacement_test${i}_it${j}.log"
+                if [[ "{$i}" == 2 ]]
+                then
+                        "$ioBin" \
+                                -i "${curr_test}.input.def" \
+                                -h 3 \
+                                -v 4 \
+                                -l 1 \
+                                -o out.def > "$ioLogDir"
+                elif [[ "{$i}" == 3 ]]
+                then
+                        "$ioBin" \
+                                -i "${curr_test}.input.def" \
+                                -h 7 \
+                                -v 6 \
+                                -l 1 \
+                                -o out.def > "$ioLogDir"
+                elif [[ "{$i}" == 4 ]] || [ "{$i}" == 5 ]
+                then
+                        "$ioBin" \
+                                -i "${curr_test}.input.def" \
+                                -h 7 \
+                                -v 8 \
+                                -l 1 \
+                                -o out.def > "$ioLogDir"
+                else
+                        "$ioBin" \
+                                -i "${curr_test}.input.def" \
+                                -h 8 \
+                                -v 7 \
+                                -l 1 \
+                                -o out.def > "$ioLogDir"
+                fi
+
+                # Moving the output DEF of IOPlacement to the ISPD18
+                # directory. With that, this out DEF is the new input DEF in
+                # the next iteration
+                mv out.def "$curr_test".input.def
+        done
+done
