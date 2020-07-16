9a5e9e38e2c1c8b783604e5b9a4344a2cb4cd4db
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-07-16 16:41:16 -0300

    Fix unit tests scripts

--- test/src/test_rand/ioPlacer2.tcl
+++ test/src/test_rand/ioPlacer2.tcl
@@ -1,7 +1,7 @@
 read_lef input.lef
 read_def input.def
 
-io_placer -hor_layer 2 -ver_layer 3 -random -random_seed 56
+io_placer -hor_layer 2 -ver_layer 3 -boundaries_offset 0 -random -random_seed 56
 write_def "result2.def"
 
 exit
